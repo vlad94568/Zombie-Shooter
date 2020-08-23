6c1b3dcd36ef0475cfbe14e639482924d3ef3570
Vlad Ivanov <vlad94568@gmail.com>
2020-08-22 19:04:26 -0700

    Bullet - zombie WIP.

---
+++ Assets/ZombieHealthScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ZombieHealthScript : MonoBehaviour
+{
+
+    public float health = 100f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+         if (collision.gameObject.tag == "Bullet")
+         {
+
+             Debug.Log("Bullet Hit Zombie.");
+
+         }
+
+    }
+
+
+
+}
