7870c5b06e7901ddd01435506389880b22090dc8
Vlad Ivanov <vlad94568@gmail.com>
2020-08-23 12:09:06 -0700

    Made terrain script.

---
+++ Assets/EnvironmentScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentScript : MonoBehaviour
+{
+   private void OnTriggerEnter2D(Collider2D other) 
+    {
+
+        if (other.tag == "Bullet")
+        {
+            Object.Destroy(other.gameObject);
+        }
+
+    }
+}
