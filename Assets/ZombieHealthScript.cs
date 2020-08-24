0bea257f146fa6004edf7c7b340d0a490b8a87c8
Vlad Ivanov <vlad94568@gmail.com>
2020-08-24 15:10:25 -0700

    Zombie BOOM and update.

--- Assets/ZombieHealthScript.cs
+++ Assets/ZombieHealthScript.cs
@@ -5,11 +5,17 @@ public class ZombieHealthScript : MonoBehaviour
 
     public float health = 100f;
 
+    public GameObject explosion;
+
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
 
         if (other.tag == "Bullet")
         {
+            GameObject boom = Instantiate(explosion, this.transform.position, Quaternion.identity);
+            boom.GetComponent<ParticleSystem>().Play();
+
             Object.Destroy(this.gameObject);
             Object.Destroy(other.gameObject);
         }
