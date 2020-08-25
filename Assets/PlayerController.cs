48fb4975e20c1f86c1cf7e10046b42df55c4702a
Vlad Ivanov <vlad94568@gmail.com>
2020-08-24 21:08:06 -0700

    Added health bar.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed = 12f;
 
+    public float currentHealth = 100f;
+
+    public Slider healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Move this to when the player takes damage.
+        healthBar.value = currentHealth;
+
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
