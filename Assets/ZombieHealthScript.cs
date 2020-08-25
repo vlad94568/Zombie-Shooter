e76fb00141eaf60987eb20a6d2b95b8f978010b7
Vlad Ivanov <vlad94568@gmail.com>
2020-08-24 17:14:02 -0700

    Added zombie sound effects.

--- Assets/ZombieHealthScript.cs
+++ Assets/ZombieHealthScript.cs
@@ -6,17 +6,23 @@ public class ZombieHealthScript : MonoBehaviour
 
     public GameObject explosion;
 
+    public AudioSource dead;
+    public AudioSource hit;
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.tag == "Bullet")
         {
             health -= other.GetComponent<BulletScript> ().damage;
 
+            hit.Play();
+
             Object.Destroy(other.gameObject);
 
             if (health <=0)
             {
                 GameObject boom = Instantiate(explosion, this.transform.position, Quaternion.identity);
+                dead.Play();
                 boom.GetComponent<ParticleSystem>().Play();
                 Object.Destroy(this.gameObject);
             }
