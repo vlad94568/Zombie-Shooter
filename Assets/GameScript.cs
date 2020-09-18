32d6f1b8394222c79b0608edb8034f4eade57ba8
Vlad Ivanov <vlad94568@gmail.com>
2020-09-17 21:58:23 -0700

    Added zombie spawning

---
+++ Assets/GameScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+
+    float time = 0f;
+
+    public Transform basicZombie;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time >= 5)
+        {
+            Instantiate(basicZombie, new Vector3(Random.Range(-22, 32), Random.Range(8, -18)), basicZombie.rotation);
+            time = 0;
+        }
+    }
+}
