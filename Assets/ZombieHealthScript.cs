1ad8fb128ae3da117a46df0241e8ec9b705cc836
Vlad Ivanov <vlad94568@gmail.com>
2020-09-19 12:16:19 -0700

    Added Score

--- Assets/ZombieHealthScript.cs
+++ Assets/ZombieHealthScript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ZombieHealthScript : MonoBehaviour
 {
@@ -9,7 +10,10 @@ public class ZombieHealthScript : MonoBehaviour
     public GameObject hitExplosion;
 
     public AudioSource dead;
+
     public AudioSource hit;
+
+    public GameObject scoreObject;
     
     private void OnTriggerEnter2D(Collider2D other) 
     {
@@ -30,6 +34,10 @@ public class ZombieHealthScript : MonoBehaviour
                 AudioSource death = Instantiate(dead, this.transform.position, Quaternion.identity);
                 death.Play();
                 boom.GetComponent<ParticleSystem>().Play();
+
+                ScoreScript scoreScript = scoreObject.GetComponent<ScoreScript>();
+                scoreScript.score += 10.0f;
+
                 Object.Destroy(this.transform.parent.gameObject);
             }
 
