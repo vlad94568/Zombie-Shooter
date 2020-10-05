57575c50de58180ef04220269f098bd95cd0b744
Vlad Ivanov <vlad94568@gmail.com>
2020-10-04 20:44:33 -0700

    Added random zombie generation.

--- Assets/GameScript.cs
+++ Assets/GameScript.cs
@@ -13,6 +13,8 @@ public class GameScript : MonoBehaviour
 
     public GameObject strongZombie;
 
+    float random;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,8 +27,34 @@ public class GameScript : MonoBehaviour
 
         if (time >= 5)
         {
-            GameObject BasicClone = Instantiate(basicZombie, new Vector3(Random.Range(-22, 32), Random.Range(8, -18)), basicZombie.transform.rotation);
-            BasicClone.SetActive(true);
+            
+            random = Random.Range(1, 9);
+            Debug.Log(random);
+
+            //If statment for the normal zombie.
+
+            if (random == 1 || random == 2 || random == 3 || random == 4)
+            {
+                GameObject clone = Instantiate(basicZombie, new Vector3(Random.Range(-22, 32), Random.Range(8, -18)), basicZombie.transform.rotation);
+                clone.SetActive(true);
+            }
+
+            //If statment for the fast zombie.
+
+            else if(random == 5 || random == 6)
+            {
+                GameObject clone = Instantiate(fastZombie, new Vector3(Random.Range(-22, 32), Random.Range(8, -18)), fastZombie.transform.rotation);
+                clone.SetActive(true);
+            }
+
+            //If statment for the strong zombie.
+
+            else if(random == 7 || random == 8)
+            {
+                GameObject clone = Instantiate(strongZombie, new Vector3(Random.Range(-22, 32), Random.Range(8, -18)), strongZombie.transform.rotation);
+                clone.SetActive(true);
+            }
+
             time = 0;
         }
     }
